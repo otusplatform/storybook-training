31e23fd4dc3e7a75d5082a1d12b9399b4e1571e0
Colt Pini <colt@otus.com>
2022-01-11 12:22:43 -0700

    updating to latest version of storybook and adding the utils to use.

--- libs/docs-utils/src/lib/utils/utils.ts
+++ libs/docs-utils/src/lib/utils/utils.ts
@@ -1,7 +1,7 @@
-export const getModuleMetadata = (module) =>
+// eslint-disable @typescript-eslint/no-explicit-any
+export const getModuleMetadata = (module: any) =>
   Reflect?.getOwnPropertyDescriptor(module, '__annotations__')?.value[0];
 
-// eslint-disable-next-line @typescript-eslint/ban-types
 const propsToArray = (props: object): any[] => {
   const arr: string[] = [];
   for (const prop in props) {
@@ -10,15 +10,31 @@ const propsToArray = (props: object): any[] => {
   return arr;
 };
 
-export const defaultTemplate = ({ name, args, tag = '', inject = '', selfClosing = false }) => {
+export const defaultTemplate = ({
+  name,
+  args,
+  ngContent,
+  tag = '',
+  inject = '',
+  selfClosing = false,
+}: {
+  name: string;
+  args: any;
+  ngContent?: string;
+  tag?: string;
+  inject?: string;
+  selfClosing?: boolean;
+}) => {
   const start = tag ? `${tag} ${name}` : name;
   const props = propsToArray(args)
     .map((arg) =>
-      arg.indexOf('on') === 0 ? `(${arg.substring(2)})="${arg}"` : `[${arg}]="${arg}" `
+      arg.indexOf('on') === 0
+        ? `(${arg.substring(2)})="${arg}"`
+        : `[${arg}]="${arg}" `
     )
     .join('');
 
-  let end = `>${args.ngContent ? args.ngContent : ''}</${tag ? tag : name}`;
+  let end = `>${ngContent ? ngContent : ''}</${tag ? tag : name}`;
   if (selfClosing) {
     end = ' /';
   }
