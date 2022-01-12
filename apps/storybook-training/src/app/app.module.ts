b346b0f553010adc7e97ce44dafae34e73a75841
Colt Pini <colt@otus.com>
2022-01-12 09:02:23 -0700

    removing component, this snuck in.

--- apps/storybook-training/src/app/app.module.ts
+++ apps/storybook-training/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
